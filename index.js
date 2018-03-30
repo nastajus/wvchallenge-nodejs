5f60f759cbbe7d8dbb2a85ad3ecbb25e6d6bba57
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-30 17:21:55 -0400

    accepts file uploads, prints to console, simply confirms in-browser.

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ const express = require('express');
 const port = process.env.PORT || 3005;
 const ejs = require('ejs');
 const path = require('path');
+const formidable = require ('express-formidable');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const csvjson = require('csvjson');
 
 const app = express();
+
+//ensures empty folder `upload` exists without fuss.
+mkdirp(path.join(__dirname, 'uploads'));
 
 // Set the default templating engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(formidable({uploadDir: path.join(__dirname, 'uploads')}));
 
 app.get('/', function (req, res) {
 	res.render('index.ejs', {} );
 });
 
 app.post('/test', function (req, res) {
+	console.log(req.fields); // contains non-file fields
+	console.log(req.files); // contains files
+
+	let data = fs.readFileSync(req.files.expenseFile.path, { encoding : 'utf8'});
+	let expenseItemsFile = csvjson.toObject(data, { delimiter: ',', quote: '"' });
+	console.log(expenseItemsFile);
+
 	res.send('test response');
 });
 
